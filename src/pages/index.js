d2bca08c1541935dccfac6dbab00f7026dcde4df
Ethan <ethan.nordahl1@gmail.com>
2023-04-30 04:58:17 -0700

    no widgets

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,6 @@ import Modal from "../components/Modal";
 import { getProviders, getSession, useSession } from "next-auth/react";
 import { useRecoilState } from "recoil";
 import { modalState } from "../../atoms/modalAtom";
-import Widgets from "../components/Widgets";
 
 export default function Home({ trendingResults, followResults, providers }) {
   const { data: session } = useSession();
@@ -26,11 +25,6 @@ export default function Home({ trendingResults, followResults, providers }) {
       <main className="bg-black min-h-screen flex max-w-[1500px] mx-auto">
         <Sidebar />
         <Feed />
-        <Widgets
-          trendingResults={trendingResults}
-          followResults={followResults}
-        />
-
         {isOpen && <Modal />}
       </main>
     </div>
