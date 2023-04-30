5912b6d4e62aaf151e3bd74c10b93442765495f2
Ethan <ethan.nordahl1@gmail.com>
2023-04-30 04:44:13 -0700

    finished

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,6 +6,7 @@ import Modal from "../components/Modal";
 import { getProviders, getSession, useSession } from "next-auth/react";
 import { useRecoilState } from "recoil";
 import { modalState } from "../../atoms/modalAtom";
+import Widgets from "../components/Widgets";
 
 export default function Home({ trendingResults, followResults, providers }) {
   const { data: session } = useSession();
@@ -25,7 +26,10 @@ export default function Home({ trendingResults, followResults, providers }) {
       <main className="bg-black min-h-screen flex max-w-[1500px] mx-auto">
         <Sidebar />
         <Feed />
-        {/* Widget */}
+        <Widgets
+          trendingResults={trendingResults}
+          followResults={followResults}
+        />
 
         {isOpen && <Modal />}
       </main>
